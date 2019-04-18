1c653e512fd2160c6d49bb660a4875ee5c996186
Andre Henn <henn@terrestris.de>
2019-04-18 11:18:42 +0200

    adds elementary test class

---
+++ packages/baseclient-components/src/container/LayerLegendAccordion/LayerLegendAccordion.spec.tsx
@@ -0,0 +1,33 @@
+/*eslint-env jest*/
+import TestUtils from '../../../spec/TestUtils';
+
+import LayerLegendAccordion from './LayerLegendAccordion';
+
+describe('<LayerLegendAccordion />', () => {
+  let map;
+  let wrapper: any;
+
+  beforeEach(() => {
+    map = TestUtils.createMap({});
+    wrapper = TestUtils.mountComponent(LayerLegendAccordion, {
+      t: () => {},
+      map: map,
+    }, {});
+  });
+
+  afterEach(() => {
+    wrapper.unmount();
+    TestUtils.unmountMapDiv();
+  });
+
+  describe('Basics', () => {
+    it('is defined', () => {
+      expect(LayerLegendAccordion).not.toBe(undefined);
+    });
+
+    it('can be rendered', () => {
+      expect(wrapper).not.toBe(undefined);
+    });
+  });
+
+});
