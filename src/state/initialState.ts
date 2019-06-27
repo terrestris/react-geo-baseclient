24008b75e623c388f9f8aca69cd8b88f51095f55
arieger <rieger@terrestris.de>
2019-06-27 14:53:05 +0200

    remove unused dummy defaults from initial state

--- src/state/initialState.ts
+++ src/state/initialState.ts
@@ -1,11 +1,11 @@
 export default {
   mapView: {
     present: {
-      center: [370000, 5546750],
+      center: [0, 0],
       zoom: 0,
       projection: 'EPSG:3857',
-      resolutions: [560, 280, 140, 70, 28, 14, 7, 2.8, 1.4, 0.7, 0.28, 0.14, 0.028],
-      mapExtent: [-20026376.39, -20048966.1, 20026376.39, 20048966.1]
+      resolutions: [],
+      mapExtent: []
     },
     past: [],
     future: []
