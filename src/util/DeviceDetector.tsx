7ae4e75933e5be374643c88ef60be28b382c0e96
blitza@terrrestris.de <blitza@terrestris.de>
2021-01-27 16:05:25 +0100

    linting

--- src/util/DeviceDetector.tsx
+++ src/util/DeviceDetector.tsx
@@ -4,13 +4,14 @@
  * @class
  */
 export default class DeviceDetector {
-    /**
+  /**
      * Check if user-Agent contains mobile browser
      */
-    static isMobileDevice () {
-        const mobileQuery = window.matchMedia('only screen and (max-width: 450px) and (orientation: portrait), only screen and (max-width: 750px) and (orientation: landscape), only screen and (max-width: 580px)');
-        const mobileNavigatorRegEx = new RegExp('/Mobile|iP(hone|od|ad)|Android|BlackBerry|IEMobile|Kindle|NetFront|Silk-Accelerated|(hpw|web)OS|Fennec|Minimo|Opera M(obi|ini)|Blazer|Dolfin|Dolphin|Skyfire|Zune/');
-        const isMobile = (mobileQuery.matches || mobileNavigatorRegEx.test(window.navigator.userAgent));
-        return isMobile;
-    }
+  static isMobileDevice () {
+    /* eslint-disable max-len */
+    const mobileQuery = window.matchMedia('only screen and (max-width: 450px) and (orientation: portrait), only screen and (max-width: 750px) and (orientation: landscape), only screen and (max-width: 580px)');
+    const mobileNavigatorRegEx = new RegExp('/Mobile|iP(hone|od|ad)|Android|BlackBerry|IEMobile|Kindle|NetFront|Silk-Accelerated|(hpw|web)OS|Fennec|Minimo|Opera M(obi|ini)|Blazer|Dolfin|Dolphin|Skyfire|Zune/');
+    const isMobile = (mobileQuery.matches || mobileNavigatorRegEx.test(window.navigator.userAgent));
+    return isMobile;
+  }
 }
