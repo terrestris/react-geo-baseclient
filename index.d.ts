85950078a4adaac0d839bef6f974a882670795bc
Daniel Koch <koch@terrestris.de>
2021-09-15 14:37:59 +0200

    Remove redux-async-initial-state dependency

--- index.d.ts
+++ index.d.ts
@@ -5,5 +5,4 @@ declare module '@terrestris/ol-util/*';
 declare module '@terrestris/vectortiles';
 declare module 'react-i18next';
 declare module 'redux-undo';
-declare module 'redux-async-initial-state';
 
