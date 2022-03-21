86148a291545399fcf7e0268774d78c40dcedcb6
Daniel Koch <koch@terrestris.de>
2022-03-21 15:52:52 +0100

    Remove unneeded import

--- config/jest/spec/setup.js
+++ config/jest/spec/setup.js
@@ -3,8 +3,6 @@ import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 import '@babel/polyfill';
 import 'jest-canvas-mock';
 
-require('es6-promise').polyfill();
-
 global.window = {};
 global.fetch = jest.fn(() => new Promise(resolve => resolve()));
 
