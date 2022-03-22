91ab62e64d1423cac80daafb01ae16641d8e4113
Daniel Koch <koch@terrestris.de>
2022-03-22 12:00:44 +0100

    Remove redux-undo

--- index.d.ts
+++ index.d.ts
@@ -4,5 +4,3 @@ declare module '@terrestris/ol-util';
 declare module '@terrestris/ol-util/*';
 declare module '@terrestris/vectortiles';
 declare module 'react-i18next';
-declare module 'redux-undo';
-
